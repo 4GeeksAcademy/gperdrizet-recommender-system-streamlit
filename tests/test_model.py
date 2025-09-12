e7908e91b2798d8ad34b69502fecd43cb1f1696a
George Perdrizet <george@perdrizet.org>
2025-09-12 21:57:05 +0000

    Cleaned up

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,11 +1,12 @@
-import src.movie_recommender as recommender
 
+
+import src.movie_recommender as recommender
 
 def test_model_output():
     '''Tests to see if the model is giving sane output.'''
 
     # Call then recommendation function with an input for which we know the expected output
-    similar_movies=recommender.get_movie_recommendations(
+    similar_movies = recommender.get_movie_recommendations(
         'Star Wars',
         recommender.MODEL,
         recommender.TFIDF_MATRIX,
